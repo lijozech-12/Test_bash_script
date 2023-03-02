e4fec19ee309ad569e86d322e019332a5b538510
Lijo Zechariah <lijozechariah.mec@gmail.com>
2023-03-02 20:35:00 +0530

    added pipeline to 2nd script

--- mqtt_test.sh
+++ mqtt_test.sh
@@ -5,4 +5,7 @@ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> /root/.bashrc
 source /root/.bashrc
 echo "succeful"
+wget https://raw.githubusercontent.com/lijozech-12/Test_bash_script/main/mqtt_test2.sh
+chmod +x mqtt_test2.sh
+./mqtt_test2
 
